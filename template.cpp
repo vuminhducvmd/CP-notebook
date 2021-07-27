49027a432954fc9a0dd3f7a9cc668f5b86ffd1fd
vuminhducvmd <51254313+vuminhducvmd@users.noreply.github.com>
2021-07-27 14:48:06 +0700

    Create template.cpp

---
+++ template.cpp
@@ -0,0 +1,29 @@
+#include <bits/stdc++.h>
+using namespace std;
+ 
+#define pb push_back
+#define st first
+#define nd second
+#define lb lower_bound
+#define ub upper_bound
+#define bit(x, i) ((x>>i)&1)
+#define x real()
+#define y imag()
+ 
+typedef long long ll;
+typedef long double ld;
+typedef complex<ll> pt;
+ 
+const ld EPS=0.00000001;
+const ll INF=1e18;
+const ll mod=1e9+7;
+
+signed main() {
+  ios_base::sync_with_stdio(0);
+  cin.tie(0); cout.tie(0);
+  
+  freopen(".inp","r",stdin);
+  freopen(".out","w",stdout);
+    
+  return 0;
+}
